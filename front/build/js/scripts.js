5dca677d48922b4907c797942f860ce44d954888
Yakup <yakupjoraev@gmail.com>
2023-07-08 14:37:35 +0300

    added new block and newe progress bar

--- front/build/js/scripts.js
+++ front/build/js/scripts.js
@@ -32,5 +32,39 @@ function simpleSelect() {
 simpleSelect();
 
 
+function copyReferal() {
+  const container = document.querySelector('.referal__link');
+
+  if (!container) {
+    return null
+  }
+
+  const toolt = document.querySelector('.referal__link-content');
+  const text = document.querySelector('.referal__link-text')
+  const span = toolt.querySelector('.tlt');
+
+
+  function showToolTip(e) {
+    span.classList.add('show');
+    if (e.type == "click") {
+      span.innerText = "copied"
+      let range = document.createRange();
+      range.selectNode(this.querySelector('.referal__link-text'));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+    }
+  }
+
+  function hideToolTip() {
+    span.classList.remove('show');
+    span.innerText = "Copt to Clipboard"
+  }
+
+  toolt.addEventListener('click', showToolTip)
+  toolt.addEventListener('mouseenter', showToolTip)
+  toolt.addEventListener('mouseleave', hideToolTip)
+}
+
+copyReferal();
 
 AOS.init();
